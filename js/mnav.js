a00e42f519919e4712933d2b814c363e3fb5e631
david-lehto-johanneberg <60431157+david-lehto-johanneberg@users.noreply.github.com>
2020-04-29 22:34:56 +0200

    Hemsidan färdig

---
+++ js/mnav.js
@@ -0,0 +1,16 @@
+(function() {
+
+	var hamburger = {
+		mnavToggle: document.querySelector('.mnav-toggle'),
+		mnav: document.querySelector('mnav'),
+
+		doToggle: function(e) {
+			e.preventDefault();
+			this.mnavToggle.classList.toggle('expanded');
+			this.mnav.classList.toggle('expanded');
+		}
+	};
+
+	hamburger.mnavToggle.addEventListener('click', function(e) { hamburger.doToggle(e); });
+
+}());
